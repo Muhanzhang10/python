aca131fe406ad2de5c1a6dad70a0f7470fcc1cce
Muhanzhang10 <60680550+Muhanzhang10@users.noreply.github.com>
2021-05-15 14:36:19 +0800

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 29 15:50:10 2020
+
+@author: zhangmuhan
+"""
+
+#第一题
+word1 = "Hello"
+word2 = "World"
+sentence1 = ["Hello", "World"]
+sentence2 = ["I", "am", "in", "dire", "need", "of", "a", "break", "."]
+#（1）根据 word1, word2 内容,通过字符串加法，打印 "Hello World"
+#（2）根据 sentence2 内容,通过字符串加法，打印 "Hello World"
+#（3）根据 sentence1 内容，通过字符串加法，将整句话打印
+
+
+#第二题
+num1 = [1, 5, 3]
+num2 = [6, 2, 4]
+#（1）根据 num1, num2内容，通过列表加法，将两个列表合并，并定义为num3，并打印
+#（2）根据 num3内容，将num3数字从小到大排序，再将数字从大到小排序（参考列表的sort函数）
+# (3) 提取num3中最大数字和最小数字并打印
+
+#第三题
+#写一段程序，当运行时需要在控制台实现以下交互功能：
+#控制台输入数字（参考input函数，注意input函数默认type为str）
+#若此数字小于100且大于0 打印 "valid input"，相反，则打印 "invalid input"
+#若此数字等于50，打印 “correct input”,若不然，则打印 “incorrect input”
+
+#第四题
+#定义阶乘函数
+#输入大于0整数x，返还 x*(x-1)*(x-2)* ... 2*1
+#hint：使用递归思路
+
+#第五题
+#定义斐波那契数函数
+#a0 = 0, a1 = 1, an = a(n-1) + a(n-2)
+#序列前几个数为 0, 1, 1, 2, 3, 5, 8, 13...
+#输入大于等于0整数n, 返还第n个斐波那契数
+#hint: 使用递归思路
+
+#第六题
+#定义以下函数
+#输入整数n,判断是否存在正整数pow使得 2(pow) == n，若有，返还True，反之则返还 False
+#例：2， 4， 16， 64返还True, 1, 6, 7, 10返还False
+ 
+
+
